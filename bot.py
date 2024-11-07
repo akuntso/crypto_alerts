f503363f8502b9174d00ea7905bb7d89b5ed5e76
Andrii Kuntso <andrii.kuntso@globallogic.com>
2024-11-07 18:56:40 +0200

    Added function descriptions and comments

--- bot.py
+++ bot.py
@@ -23,25 +23,33 @@ FTN_INTR_PRICE = 15
 CHG_IO = '5'
 
 class TelegramBot:
+    """Class that describes main functionality for Telegram Bot, that is fetching data from web,
+    sending textdata or screenshots, writing textdata to the file and saving screenshots.
+    Added logging of actions.
 
-    def __init__(self):
+    """
+    def __init__(self) -> None:
+        """Initialisation vars and logging"""
         self.bot_token = BOT_TOKEN
         self.chat_id = CHAT_ID
         self.percentage = None
         self.filters = None
 
-
         logging.basicConfig(
             level=logging.INFO,
             format='%(asctime)s - %(levelname)s - %(message)s',
             handlers=[
-                logging.StreamHandler(),  # Логування у консоль
-                logging.FileHandler('my_log_file.log')  # Логування у файл
+                logging.StreamHandler(),  # console logging
+                logging.FileHandler('my_log_file.log')  # file logging
             ]
         )
 
-    def _handle_consent_your_personal_data_popup(self, page: Page):
-        # Обробка спливаючого вікна якщо воно появилося, знайти і натиснути кнопку "Consent"
+    def _handle_consent_your_personal_data_popup(self, page: Page) -> None:
+        """Handling Consent your personal data popup
+
+        :param page: object of Playwright page on which need to do actions
+        """
+        # if the dialog appeared, look up it and tap the button "Consent"
         try:
             logging.info("Waiting for 'Consent popup to appear'")
             page.wait_for_selector(selector='button[aria-label="Consent"]', timeout=5000)
@@ -51,23 +59,34 @@ class TelegramBot:
         except Exception:
             logging.warning("Consent popup was not appeared")
 
+    def _get_coins_data_screenshot(self, page: Page) -> None:
+        """Getting screenshot and saving it with the time name
 
-    def _get_coins_data_screenshot(self, page: Page):
+        :param page: object of Playwright page on which need to do actions
+        """
         current_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
-        # перезиписує змінну скріншот нейм, початково вона None
+        # rewriting the variable screenshot_name, initialy it names None
         self.screenshot_name = f"{self.percentage}%_{self.filters}__{current_time}.png"
         screenshots_path = os.path.join('screenshots', self.screenshot_name)
         logging.info(f"Saving screenshot - {self.screenshot_name}")
         page.screenshot(path=screenshots_path)
 
+    def _append_coins_data_to_file(self, coins_to_add: str) -> None:
+        """Adding data to the file
 
-    def _append_coins_data_to_file(self, coins_to_add):
+        :param coins_to_add: coins data from web table
+        """
         logging.info("Writing coin data to the file")
         with open('coindata.txt', 'a') as file:
             file.write(coins_to_add + '\n')
 
-
     def _set_page_and_go_to_coinglass(self, playwright: Playwright) -> Page:
+        """Setting the browser parameters and going the web page
+
+        :param playwright: Playwright object
+
+        :returns page: object of Playwright page on which need to do actions
+        """
         browser = playwright.chromium.launch(headless=False, slow_mo=1000)
         context = browser.new_context(user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
         page = context.new_page()
@@ -81,8 +100,13 @@ class TelegramBot:
         
         return page
 
-
     def _get_data_from_table(self, page: Page) -> str:
+        """Getting data from web table
+
+        :param page: object of Playwright page on which need to do actions
+
+        :returns: returns coins data from web table
+        """
         page.wait_for_selector('tbody.ant-table-tbody')
         logging.info("Waiting for data to display 2 sec")
         page.wait_for_timeout(2000)
@@ -97,7 +121,6 @@ class TelegramBot:
             logging.info(f"No Data to display on website")
             return ''
         else:
-
             current_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H-%M-%S')
             logging.info(f"Preparing data for Telegram bot")
             proceded_coins = [str(f'{coin} - https://www.coinglass.com/tv/ru/Binance_{coin}USDT') for coin in coins_from_table if coin is not None]
@@ -105,8 +128,11 @@ class TelegramBot:
 
             return coins_for_message
 
+    def _send_telegram_message(self, message: str) -> None:
+        """Sending a message to the bot
 
-    def _send_telegram_message(self, message: str):
+        :message: a message to send to the bot
+        """
         logging.warning(f"Processing message into send_telegram_bot: {message}")
         if not message:
             logging.error("No Data to sent to Telegram bot")
@@ -120,40 +146,48 @@ class TelegramBot:
         headers = {
             "Content-Type": "application/json"
         }
-
         logging.info(f"Sending message to the chat with")
         response = requests.post(url, json=payload, headers=headers)
         logging.info(f"Response is -- {response}")
-        return response.json()
+        #  return response.json()
 
-
-    def _send_telegram_photo(self):
+    def _send_telegram_photo(self) -> None:
+        """Sending screenshot to the bot"""
 
         url = f"https://api.telegram.org/bot{self.bot_token}/sendPhoto"
         logging.warning("Checking photo name if new exists")
-
         if not self.screenshot_name:
             logging.error(f"Screenshot name is -- {self.screenshot_name}")
             return
 
-        files = {'photo': open(f'screenshots/{self.screenshot_name}', 'rb')}
+        file = {'photo': open(f'screenshots/{self.screenshot_name}', 'rb')}
         logging.info("Sending Photo to the chat")
-        response = requests.post(url, files=files, data={'chat_id': self.chat_id})
+        response = requests.post(url, files=file, data={'chat_id': self.chat_id})
         logging.info(f"Response is -- {response}")
-        return response.json()
+        #  return response.json()
 
 
 class BotPrice(TelegramBot):
+    """Class describes the functionality for coin price actions"""
 
-    def __init__(self, percentage_price: str=THREE_PERC_PRICE, interval: int=FIVE_INTR_PICE):
+    def __init__(self, percentage_price: str=THREE_PERC_PRICE, interval: int=FIVE_INTR_PICE) -> None:
+        """Initialisation
+
+        :param percentage_price: price in percentagees that has increaced
+        :param interval: changing price during some time (e.g. 15min)
+
+        """
         super().__init__()
         self.percentage = percentage_price
         self.interval = interval
-        self.screenshot_name = None
-        self.filters = 'Price'
+        self.screenshot_name = None #  name for screenshot of web table to save and send
+        self.filters = 'Price' #  label that shows which filter is applying
 
+    def _apply_filter_price(self, page: Page) -> None:
+        """Applying filter for coin price
 
-    def _apply_filter_price(self, page: Page):
+        :param page: object of Playwright page on which need to do actions
+        """
         logging.info("Selecting 'Price Change'")
         page.get_by_role("button", name="Price Change").click()
         page.get_by_role("button", name=f"Price Change ({self.interval}m)").click()
@@ -163,8 +197,12 @@ class BotPrice(TelegramBot):
         page.get_by_role("button", name="Close").click()
         logging.info(f"Filter for price over {self.percentage}% was succssesfuly applied")
 
-
     def fetch_coins_with_changed_price(self) -> str:
+        """Fetching coins data from web table with changed price
+
+        :returns: coins data for a message to send
+        """
+
         with sync_playwright() as playwright:
             page = self._set_page_and_go_to_coinglass(playwright)
             self._apply_filter_price(page)
@@ -175,20 +213,20 @@ class BotPrice(TelegramBot):
 
             return coins_for_message
 
-
-    def send_telegram_photo_price(self):
-        # перезаписуємо назуву скріншота, щоб не надсилався попердній якщо такий був
+    def send_telegram_photo_price(self) -> None:
+        """Sending screenshot with changed coins price"""
+        # Rewrite the name of screenshot in order not to send previous if it has existed
         self.screenshot_name = None
         self.fetch_coins_with_changed_price()
         self._send_telegram_photo()
 
-
-    def send_telegram_message_price(self):
+    def send_telegram_message_price(self) -> None:
+        """Sending a message with changed coins price"""
         message = self.fetch_coins_with_changed_price()
         self._send_telegram_message(message)
 
-
     def run_scheduler(self):
+        """Runnig the scheduler every minutes"""
         # schedule.every(1).minutes.do(lambda: executor.submit(send_telegram_message, BOT_TOKEN, CHAT_ID, TWO_PERC))
         logging.info(f"Executing task 'send telegram message' for price changed over {self.percentage}%")
         logging.info("I'm running on thread %s" % threading.current_thread())
@@ -200,14 +238,23 @@ class BotPrice(TelegramBot):
 
 
 class BotIO(TelegramBot):
+    """Class describes the functionality for coin open interest actions"""
 
-    def __init__(self, percentage_io=CHG_IO):
+    def __init__(self, percentage_io=CHG_IO) -> None:
+        """Initialisation
+
+        :param percentage_io: open interest in percentagees that has increaced
+        """
         super().__init__()
         self.percentage = percentage_io
-        self.screenshot_name = None
-        self.filters = 'IO'
+        self.screenshot_name = None #  name for screenshot of web table to save and send
+        self.filters = 'IO' #  label that shows which filter is applying
 
-    def _apply_filter_io(self, page: Page):
+    def _apply_filter_io(self, page: Page) -> None:
+        """Applying filter for coin open interest
+
+        :param page: object of Playwright page on which need to do actions
+        """
         logging.info("Selecting 'Open Interest'")
         page.get_by_role("button", name="Open Interest").click()
         page.get_by_role("button", name="OI Change (15m)").click()
@@ -219,8 +266,11 @@ class BotIO(TelegramBot):
         page.get_by_role("button", name="Close").click()
         logging.info(f"Filter for IO over {self.percentage}% was succssesfuly applied")
 
-
     def fetch_coins_with_changed_io(self) -> str:
+        """Fetching coins data from web table with changed open interest
+
+        :returns: coins data for a message to send
+        """
         with sync_playwright() as playwright:
             page = self._set_page_and_go_to_coinglass(playwright)
             self._apply_filter_io(page)
@@ -231,19 +281,20 @@ class BotIO(TelegramBot):
 
             return coins_for_message
 
-    def send_telegram_photo_io(self):
-        # перезаписуємо назуву скріншота, щоб не надсилався попердній якщо такий був
+    def send_telegram_photo_io(self) -> None:
+        """Sending screenshot with changed coins open interest"""
+        # Rewrite the name of screenshot in order not to send previous if it has existed
         self.screenshot_name = None
         self.fetch_coins_with_changed_io()
         self._send_telegram_photo()
 
-
     def send_telegram_message_io(self):
+        """Sending a message with changed coins open interest"""
         message = self.fetch_coins_with_changed_io()
         self._send_telegram_message(message)
 
-
     def run_scheduler(self):
+        """Runnig the scheduler every minutes"""
         # schedule.every(1).minutes.do(lambda: executor.submit(send_telegram_message, BOT_TOKEN, CHAT_ID, TWO_PERC))
         logging.info(f"Executing task 'send telegram message' for IO over {self.percentage}%")
         logging.info("I'm running on thread %s" % threading.current_thread())
@@ -258,7 +309,6 @@ if __name__ == "__main__":
     # executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
     bot_price = BotPrice(percentage_price='1', interval=15)
     bot_price.run_scheduler()
-    
 
     # with ThreadPoolExecutor(max_workers=2) as executor:
     #     try:
